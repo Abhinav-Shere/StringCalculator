ec7cfcb3b222b40f982a0de95546da70b27afe80
Abhinav-Shere <abhhinavdev12@gmail.com>
2024-05-07 21:33:57 +0530

    Added case handling when numbers are negative.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,9 @@ module StringCalculator
        return 0 if string.empty?
        return string.to_i if string.length.eql?(1)
        numbers = string.scan(/-?\d+/).map(&:to_i).select { |s| s < 1001 }
+       if numbers.any? { |i| i < 0 }
+          raise 'Negatives not allowed'
+       end
        numbers.sum
     end
 end
