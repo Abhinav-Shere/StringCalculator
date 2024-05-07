9e1cf1d6458b9fcc8d1c41edcd8a4eef4a477980
Abhinav-Shere <abhhinavdev12@gmail.com>
2024-05-07 21:43:20 +0530

    Refactored the code after all test cases were successful.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,7 @@
 module StringCalculator
     def self.add(string)
-       return 0 if string.empty?
-       return string.to_i if string.length.eql?(1)
-       numbers = string.scan(/-?\d+/).map(&:to_i).select { |s| s < 1001 }
-       if numbers.any? { |i| i < 0 }
-          raise 'Negatives not allowed'
-       end
-       numbers.sum
+        raise 'Negatives not allowed' if string.match?(/-\d+/)
+      
+        string.scan(/\d+/).map(&:to_i).reject { |num| num >= 1001 }.sum
     end
 end
