414be176ecf28f9403f5e1e5fe70b0a7c4981406
Abhinav-Shere <abhhinavdev12@gmail.com>
2024-05-08 14:15:24 +0530

    Removed unnecessary line of code.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,6 @@
 module StringCalculator
     def self.add(string)
         raise 'Negatives not allowed' if string.match?(/-\d+/)
-      
         string.scan(/\d+/).map(&:to_i).reject { |num| num >= 1001 }.sum
     end
 end
